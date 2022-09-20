54c8c7a28998073aa5235b5d9ad2b867f568452c
Arshad <arshuafridi@gmail.com>
2022-09-20 16:30:35 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './components/header';
-import CartPage from './pages/cart';
-import ProductPage from './pages/product';
-import LoginPage from './pages/login';
-import HomePage from './pages/home';
+import Header from "./components/header";
+import CartPage from "./pages/cart";
+import ProductPage from "./pages/product";
+import LoginPage from "./pages/login";
+import HomePage from "./pages/home";
 
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
